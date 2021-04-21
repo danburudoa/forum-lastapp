b64ad93c4aee2476eadadb2c654d568144121143
danburudoa <yumamukai515@gmail.com>
2021-04-21 15:38:04 +0900

    boardsコントローラー記述

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,56 @@
+class BoardsController < ApplicationController
+    before_action :authenticate_user!,only:[:create,:edit,:update,:destroy]
+    before_action :set_tweet, only: [:edit, :show,:update,:destroy]
+ 
+    def index
+      @boards = Board.includes(:user)
+      @board_page = Board.all.page(params[:page]).per(10)
+    end
+    
+    def new
+       @board = Board.new
+    end
+ 
+    def create
+       @board = Board.new(board_params)
+       if @board.save
+          redirect_to root_path
+       else
+          render :new
+       end
+    end
+    
+    def show  
+       @comment = Comment.new
+       @comments = @board.comments.includes(:user)
+       @comment_page = Comment.all.page(params[:page]).per(10)
+    end
+ 
+    def edit
+       
+    end
+ 
+    def update
+       @board.update(board_params)
+       if @board.save
+          redirect_to root_path
+       else 
+          render :edit
+       end
+    end
+ 
+    def destroy
+      @board.destroy
+      redirect_to root_path
+    end
+ 
+    private
+ 
+    def board_params
+       params.require(:board).permit(:title,:text,:image).merge(user_id: current_user.id)
+    end
+ 
+    def set_tweet
+       @board = Board.find(params[:id])
+     end
+ end
