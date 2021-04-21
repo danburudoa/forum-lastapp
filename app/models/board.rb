f80f2cd2b6436f8dfe91a387633d07a862abf1c9
danburudoa <yumamukai515@gmail.com>
2021-04-21 16:08:26 +0900

    boardモデルへの記述

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,10 @@
 class Board < ApplicationRecord
+    belongs_to :user
+    has_many :comments, dependent: :destroy
+    has_many :likes
+    has_one_attached :image
+
+    validates :image, presence: true
+    validates :title, presence: true
+    validates :text,  presence: true
 end
