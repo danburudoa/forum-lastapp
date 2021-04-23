22bc068c8a47e5a7014d888d24d597c6c8e2703a
danburudoa <yumamukai515@gmail.com>
2021-04-23 23:47:11 +0900

    ページネーション修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -22,8 +22,7 @@ class BoardsController < ApplicationController
     
     def show  
        @comment = Comment.new
-       @comments = @board.comments.order(created_at: :desc).includes(:user)
-       @comment_page = Comment.all.page(params[:page]).per(10)
+       @comments = @board.comments.order(created_at: :desc).includes(:user).page(params[:page]).per(50)
     end
  
     def edit
