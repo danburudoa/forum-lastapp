fdd508ed427671406674dee1424273e39cb96176
danburudoa <yumamukai515@gmail.com>
2021-05-01 16:53:29 +0900

    タグモデル削除

--- app/models/tag.rb
+++
@@ -1,6 +0,0 @@
-class Tag < ApplicationRecord
-    has_many :board_tag_relations, dependent: :destroy
-    has_many :boards, through: :board_tag_relations, dependent: :destroy
-
-    validates :name, uniqueness: true
-end
