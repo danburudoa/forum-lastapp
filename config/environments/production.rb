7cb2733cf31579cde18bcfce794f3cd7fe74ac40
danburudoa <yumamukai515@gmail.com>
2022-09-25 18:44:05 +0900

    記述変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.url = "wss://18.179.176.136/cable" 
+  config.action_cable.url = "ws://18.179.176.136/cable" 
   config.action_cable.allowed_request_origins = ['http://18.179.176.136']
   # Code is not reloaded between requests.
   config.cache_classes = true
