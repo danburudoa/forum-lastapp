d05aea7ec0516e004f2d8b74ed9973059d831289
danburudoa <yumamukai515@gmail.com>
2022-10-11 16:51:37 +0900

    スキーマ変更

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_10_07_090044) do
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
+    t.integer "board_id"
     t.integer "comment_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
